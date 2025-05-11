cb09af96b3b2db5fc10ef345ed9219d004646336
User <User@DESKTOP-A4LHSLG>
2025-05-12 02:51:11 +0700

    Добавьте файлы проекта.

---
+++ ToolKit/App.xaml.cs
@@ -0,0 +1,71 @@
+﻿using Hardcodet.Wpf.TaskbarNotification;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace ToolKit
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private TaskbarIcon notifyIcon;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            // Создаем иконку в трее
+            notifyIcon = (TaskbarIcon)FindResource("MyNotifyIcon");
+
+            // Настройка команд
+            notifyIcon.DataContext = new Classes();
+
+            //Обработка двойного клика
+            notifyIcon.DoubleClickCommand = new RelayCommand(() =>
+            {
+                MainWindow MainWindow = new MainWindow();
+                MainWindow.Show();
+            });
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            notifyIcon.Dispose(); // Важно освободить ресурсы
+            base.OnExit(e);
+        }
+
+        private void MenuItem_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+        }
+    }
+    public class RelayCommand : ICommand
+    {
+        private readonly Action _execute;
+        private readonly Func<bool> _canExecute;
+
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        public RelayCommand(Action execute, Func<bool> canExecute = null)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter) => _canExecute?.Invoke() ?? true;
+        public void Execute(object parameter) => _execute();
+    }
+}
+
---
+++ ToolKit/Classes.cs
@@ -0,0 +1,25 @@
+﻿using Hardcodet.Wpf.TaskbarNotification;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows;
+
+namespace ToolKit
+{
+    internal class Classes
+    {
+        public ICommand SettingsCommand => new RelayCommand(
+        () =>
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+        });
+
+
+        public ICommand ExitCommand => new RelayCommand(
+            () => Application.Current.Shutdown());
+    }
+}
